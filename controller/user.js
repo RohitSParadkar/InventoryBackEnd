18a94341f3e3f9e8d9a4cc311ff309df83d6c86a
Rohit Paradkar <rparadkar@technosofteng.com>
2023-11-03 19:21:58 +0530

    single entry for user but app is crashing

--- controller/user.js
+++ controller/user.js
@@ -1,11 +1,18 @@
-const User = require('../model/user')
-exports.createUser = async(req,res)=>{
-    const {name,email,password} =req.body
-    const newUser = new User({
-       name,
-       email,
-       password
-    })
-    await newUser.save()
-    res.send(newUser)
-}
+const User = require("../model/user");
+exports.createUser = async (req, res) => {
+  const { name, email, password } = req.body;
+  //Search for user in database
+  const findUser = await User.findOne({ name });
+  if (findUser) {
+    res
+      .status(400)
+      .json({ success: false, error: "This email is already exists" });
+  }
+  const newUser = new User({
+    name,
+    email,
+    password,
+  });
+  await newUser.save();
+  res.send(newUser);
+};
--- model/user.js
+++ model/user.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose")
 const { Schema } = mongoose;
-
+const bcrypt = require('bcrypt')
 const userSchema = new Schema({
     name:{
         type:String,
@@ -21,6 +21,14 @@ const userSchema = new Schema({
         default:Date.now  //Donn't call the function
     }
 });
+//----------privous to save hash the password
+userSchema.pre('save',async function(next){
+  if(this.isModified('password')){
+    const hash = await bcrypt.hash(this.password,8)
+    this.password = hash;
+  }
+   next();
+})
 
 const User = mongoose.model("user",userSchema);
 User.createIndexes();
